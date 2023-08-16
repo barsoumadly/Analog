dda6ee10cb0976e05536bb5ee15edb95aa585494
Barsoum <barsoumadly@gmail.com>
2023-08-16 20:20:31 +0300

    update logic

--- script.js
+++ script.js
@@ -20,22 +20,28 @@ const getDate = function () {
   return [hours, minutes, seconds];
 };
 
+// Showing time
+const showTime = function (time) {
+  // Clock arrows
+  hourArrow.style.transform = `rotateZ(${
+    time[0] * 30 + (time[1] * 6) / 12
+  }deg)`;
+  minuteArrow.style.transform = `rotateZ(${time[1] * 6}deg)`;
+  secondArrow.style.transform = `rotateZ(${time[2] * 6}deg)`;
+
+  // Digital clock
+  hoursEL.textContent = String(time[0] - 12).padStart(2, 0);
+  minutesEL.textContent = String(time[1]).padStart(2, 0);
+  secondsEL.textContent = String(time[2]).padStart(2, 0);
+  stateEL.textContent = time[0] >= 12 ? 'PM' : 'AM';
+};
+
 // Setting date
 const setDate = function () {
   let time = getDate();
+  showTime(time);
   setInterval(() => {
-    // Clock arrows
-    hourArrow.style.transform = `rotateZ(${
-      time[0] * 30 + (time[1] * 6) / 12
-    }deg)`;
-    minuteArrow.style.transform = `rotateZ(${time[1] * 6}deg)`;
-    secondArrow.style.transform = `rotateZ(${time[2] * 6}deg)`;
-
-    // Digital clock
-    hoursEL.textContent = String(time[0] - 12).padStart(2, 0);
-    minutesEL.textContent = String(time[1]).padStart(2, 0);
-    secondsEL.textContent = String(time[2]).padStart(2, 0);
-    stateEL.textContent = time[0] >= 12 ? 'PM' : 'AM';
+    showTime(time);
     time = getDate();
   }, 1000);
 };
